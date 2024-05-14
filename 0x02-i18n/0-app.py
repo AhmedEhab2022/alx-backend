6e07ae4fe3d24f10069de1cba35559513096154e
AhmedEhab2022 <am8122004@gmail.com>
2024-05-14 14:09:00 +0300

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+
+""" Module to start a Flask web application
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Displays the message """
+    render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='', port=5000)
