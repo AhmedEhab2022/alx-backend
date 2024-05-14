773483380edd826c9f7d512ea920e3cbfac90e06
AhmedEhab2022 <am8122004@gmail.com>
2024-05-14 17:55:44 +0300

    Task 6

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Module to start a Flask web application
+    with babel to handle i18n
+"""
+from flask import request, render_template, Flask
+from flask_babel import Babel, _, g
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ Class to set the configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@babel.localeselector
+def get_locale():
+    """ Function to determine the best match with our supported languages.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """ Function to get a user
+    """
+    user_id = request.args.get('login_as')
+    try:
+        return users.get(int(user_id))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ Function to get a user
+    """
+    user = get_user()
+    g.user = user
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Render the main template """
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
