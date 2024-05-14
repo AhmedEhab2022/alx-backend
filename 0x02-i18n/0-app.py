dd0a72e3a099f6012977a5ab75a02211d5971ccd
AhmedEhab2022 <am8122004@gmail.com>
2024-05-14 14:13:59 +0300

    add interpreter path

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 """ Module to start a Flask web application
 """
 from flask import Flask, render_template
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """ Displays the message """
+    """ Function to render a template """
     render_template('0-index.html')
 
 
