f6e0dc51f6d5114e8659e4c549ee12788dea9557
AhmedEhab2022 <am8122004@gmail.com>
2024-05-14 17:12:46 +0300

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,5 +36,11 @@ def hello_world():
     return render_template('4-index.html')
 
 
+@app.route('/<locale>', strict_slashes=False)
+def hello_world_locale(locale):
+    """ Function to render a template """
+    return render_template('4-index.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
